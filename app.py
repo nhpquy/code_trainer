8e8caf0cfab0f5b19d39c2643e57b2a2e535ee27
takeitez <nhpquyit@gmail.com>
2020-12-04 21:53:33 +0700

    + update front-end.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 
-from flask import Flask, jsonify, request, Response, abort, render_template
+from flask import Flask, request, Response, abort, render_template
 
 from crawl_spiders import crawl_spiders
 from named_entity_recognition import train
@@ -11,9 +11,35 @@ from utils import get_input_file, get_scracy_dir
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/", methods=["GET"])
-def get():
-    return jsonify({"status": "ok"})
+@app.route("/api/v1/process", methods=["POST"])
+def crawl_and_train():
+    request_body = request.json
+    crawl_type = request_body["type"]
+
+    spider = crawl_spiders[crawl_type]
+    if spider is None:
+        abort(406, 'Not support yet')
+
+    crawl_output = get_input_file(crawl_type)
+
+    crawl_start_time = time.time()
+    p = subprocess.Popen(['scrapy', 'crawl', crawl_type, "-o", crawl_output], cwd=get_scracy_dir())
+    p.wait()
+    crawl_end_time = time.time() - crawl_start_time
+
+    train_start_time = time.time()
+    train_output = train.main(input_file=crawl_output)
+    train_end_time = time.time() - train_start_time
+
+    result = {
+        'crawl_time': crawl_end_time,
+        'train_time': train_end_time,
+        'crawl_output': crawl_output,
+        'train_output': train_output
+    }
+    body = ResponseBody(0, result).to_json()
+    response = Response(body, status=200, mimetype="application/json")
+    return response
 
 
 @app.route("/api/v1/crawl", methods=["POST"])
@@ -59,7 +85,12 @@ def train_data():
     return response
 
 
-@app.route('/draw', methods=['GET', 'POST'])
+@app.route('/visualize', methods=['GET', 'POST'])
+def visualize():
+    return render_template('visualize.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
